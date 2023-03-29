d0da77c5f700c5c1a51c58f698282de3c3cc348f
John McDove <96707236+illusies@users.noreply.github.com>
2023-03-28 20:22:46 -0500

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,48 +1,25 @@
 #!/usr/bin/python3
-# Check if n locked boxes with keys inside can all be opened
-# starting only with the 0th box unlocked.
+"""A method that determines if all the boxes can be opened"""
 
 
 def canUnlockAll(boxes):
-    if not boxes:
+    """
+    Return: True or False
+    """
+    a = len(boxes)
+    stack = [0]
+    unlocked_box = [1] + [0] * (a - 1)
+    i = 0
+
+    if a == 0:
         return True
-    opened = {0}
-    keys = [0]
-
-    while keys:
-        for key in boxes[keys.pop()]:
-            if 0 <= key < len(boxes) and key not in opened:
-                opened.add(key)
-                keys.append(key)
-
-    return len(boxes) == len(opened)
-
-
-if __name__ == '__main__':
-    good_cases = [
-        [[1], [2], [3], [4], []],
-        [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]],
-        [[2], [3, 4], [1, 5]],
-        [[2]],
-        [[0, 1], [2, 3], [7, 4], [2]],
-        [[0, 4, 4], [5], [3], [1], [2], [5]],
-        [[]],
-        [[0, 1], [2, 3], [7, 4], [2]],
-    ]
-    for case in good_cases:
-        assert canUnlockAll(case), "boxes = {}".format(str(case))
-
-    bad_cases = [
-        [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]],
-        [[1, 3], [3, 0, 1], [2], [0]],
-        [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6], [7]],
-        [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6], [7]],
-        [],
-        [[1, 2, 3], [], [], [], []],
-        [[1, 4], [2], [0, 4, 1], [6], [3], [4, 1], [5, 6], [7]],
-        [[1], [1], [1]],
-        [[], [2], [4]],
-        [[], [1, 2, 7], [0, 3]]
-    ]
-    for case in bad_cases:
-        assert canUnlockAll(case) is False, "boxes = {}".format(str(case))
+    while stack:
+        j = stack.pop()
+        for index in boxes[j]:
+            if index > 0 and index < a and unlocked_box[index] == 0:
+                unlocked_box[index] = 1
+                stack.append(index)
+        i = i + 1
+    if 0 in unlocked_box:
+        return False
+    return True
