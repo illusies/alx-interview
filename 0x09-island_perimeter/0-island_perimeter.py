7e395467e7788623c8d7f7b812d48c169a691221
John McDove <96707236+illusies@users.noreply.github.com>
2023-05-28 05:16:46 -0500

    0x09. Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+A function def island_perimeter(grid): that returns the perimeter
+of the island described in grid:
+- grid is a list of list of integers
+- grid is rectangular with its width and height not exceeding 100
+"""
+
+
+def island_perimeter(grid):
+    """ Function to find the perimeter of an island """
+    if not grid or any(type(arr) != list for arr in grid):
+        return 0
+    width = len(grid[0]) - 1
+    if any(len(arr) != width + 1 for arr in grid):
+        return 0
+    islands = connections = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                islands += 1
+                if j < len(grid[0]) - 1 and grid[i][j + 1] == 1:
+                    connections += 1
+                if i < len(grid) - 1 and grid[i + 1][j] == 1:
+                    connections += 1
+    return 4 * islands - 2 * connections
+
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid) == 12)
+    assert(island_perimeter(grid) == 12)
